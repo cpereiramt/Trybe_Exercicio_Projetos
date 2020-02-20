5ad888b36c032e7547a5fe5ab999f6dca87b4b58
Clayton Pereira <cpereiramt@gmail.com>
2020-02-20 17:51:53 -0400

    Salvando as respostas do exercicio 01  e corrigindo o css do exercicio do bloco anterior

---
+++ exercises/4_1/script.js
@@ -0,0 +1,20 @@
+
+a = 10;
+b = 6;
+
+let adicao = a + b;
+
+console.log("O valor da adição é " + adicao);
+
+let subtracao = a- b ;
+
+console.log("O valor da subtração é  " + subtracao);
+
+let multiplicacao = a * b ;
+console.log("O valor da Multiplicação é " + multiplicacao);
+
+let divisao = a / b;
+console.log("O valor da divisão é " + divisao);
+
+let modulo = a % b ;
+console.log("O resto da divisão é " + modulo);
