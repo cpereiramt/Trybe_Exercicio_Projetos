52eca63c707b380cb5db961cd3a1a160414cc458
Clayton Pereira <cpereiramt@gmail.com>
2020-02-20 18:29:49 -0400

    Salvando as respostas do exercicio 04

--- exercises/4_1/script.js
+++ exercises/4_1/script.js
@@ -60,3 +60,18 @@ else if(numy1 > numx1 && numy1 > numz1){
 
 
 }
+
+
+console.log("Exercicio 04 =======================================================================");
+
+let numberType = null;
+let number = 0 ;
+
+if(number > 0 ) {
+console.log("Positive");
+} else if(number < 0) {
+console.log("Negative");
+} else{
+console.log("Zero")
+
+}
