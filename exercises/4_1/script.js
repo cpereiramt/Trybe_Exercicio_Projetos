c54871ff29e28f9f49f22509b84742527beacb74
Clayton Pereira <cpereiramt@gmail.com>
2020-02-21 10:32:20 -0400

    Salvando alterações do exercicio 05

--- exercises/4_1/script.js
+++ exercises/4_1/script.js
@@ -75,3 +75,17 @@ console.log("Negative");
 console.log("Zero")
 
 }
+
+console.log("Exercicio 05 =======================================================================");
+
+let tangulo1 = 30;
+let tangulo02 = 100;
+let tangulo03 = 50;
+
+if(tangulo1 + tangulo02 + tangulo03 == 180)
+{
+    console.log("triangulo valido !");
+}else{
+
+    console.log("triangulo inválido !");
+}
