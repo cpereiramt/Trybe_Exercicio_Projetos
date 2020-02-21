f6014ab0636bd3106c00e0a66352faf46cc9dc88
Clayton Pereira <cpereiramt@gmail.com>
2020-02-21 11:34:39 -0400

    Salvando alterações do exercicio 06

--- exercises/4_1/script.js
+++ exercises/4_1/script.js
@@ -89,3 +89,26 @@ if(tangulo1 + tangulo02 + tangulo03 == 180)
 
     console.log("triangulo inválido !");
 }
+
+console.log("Exercicio 06 =======================================================================");
+let response = prompt("Qual a peça do xadrez ?");
+switch (response.toLowerCase()) {
+    case "rei":
+        console.log("Movimenta em qualquer direção, uma casa por vez !");        
+        break;
+    case "dama":
+        console.log("Movimenta em qualquer direção, quantas casa tiverem desde que todas livres !");
+        break;
+    case "torre":
+        console.log("Move-se em linha reta, na vertical ou horizontal !");
+        break;
+    case "bispo":
+            console.log("Move-se em na diagonal !");
+            break;        
+    case "cavalo":
+         console.log("unica peça que pode pular outras, move-se em 'L'");
+         break;
+    default:
+        console.log("Peça não encontrada!");
+        break;
+}
