cfb6f59baabe1ef951fea747820347ad7f01d47b
Clayton Pereira <cpereiramt@gmail.com>
2020-02-21 12:36:16 -0400

    refatorando e criando um arquivo separado para cada exercicio, e salvando as alterações do exercicio 07

--- exercises/4_1/script7.js
+++ exercises/4_1/script7.js
@@ -5,7 +5,10 @@ console.log("Exercicio 07 ======================================================
 
 let porcentage = prompt("digite a porcentagem da nota" )
 
-
+ if(porcentage < 0 || porcentage > 100){
+    console.log("Escolha um valor entre 0 e 100 .")
+      
+    }else{
  if (porcentage >= 90 && porcentage < 100){
         console.log("A");
 
@@ -27,10 +30,7 @@ else if(porcentage >= 50){
   else if (porcentage < 50){
         console.log("F") ;       
        }
-       else if(porcentage < 0 && porcentage > 100){
-       console.log("Escolha um valor entre 0 e 100 .")
-         
-       }
+      
     else{
          console.log("Valor inválido !");
-        }
+        }}
