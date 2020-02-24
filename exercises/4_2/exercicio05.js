8fbaf5ad5c69c2085196eca6043d80bdfa241a0b
Clayton Pereira <cpereiramt@gmail.com>
2020-02-24 18:51:52 -0400

    Salvando as alteraçoes dos exercicios 05, embora nao tenha conseguido fazer funcionar corretamente ainda

--- exercises/4_2/exercicio05.js
+++ exercises/4_2/exercicio05.js
@@ -6,14 +6,18 @@
             for(let i = 1; i <= size; i++ ){
                 for(let j = 1; j <= size; j++){
                     if(i + j > size){
-                        if(i % 2 != 0){
+                        if(i % 2 != 0)
+                         if(j + i ==0 || j + i == 2 * i - 1)
+                        {
                         text = text.concat("*");
                     }
                     }else{
-                        text = text.concat("")
+                        text = text.concat("r")
                     }
                 }
                 text = text.concat("\n")
             }
 
             console.log(text)
+
+  
