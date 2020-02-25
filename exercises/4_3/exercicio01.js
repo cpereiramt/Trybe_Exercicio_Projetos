37ef7b9bec22cdd2ffc7103f66f2ca3294bde01a
Clayton Pereira <cpereiramt@gmail.com>
2020-02-24 20:42:34 -0400

    Salvando as respostas dos exercicios do bloco 04 dia 03

---
+++ exercises/4_3/exercicio01.js
@@ -0,0 +1,16 @@
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+
+  //Exercicio numero 01 
+
+  console.log("Bem vinda ," , info.personagem);
+
+   //Exercicio numero 02
+   
+   info.recorrente="sim";
+   console.log(info)
+
+   
