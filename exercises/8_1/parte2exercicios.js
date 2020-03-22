476e7197eafc928b49841262ff785481947b84ba
Clayton Pereira <cpereiramt@gmail.com>
2020-03-22 18:44:40 -0400

    Salvando as alteraçoes do exercicio 1 do projeto

---
+++ exercises/8_1/parte2exercicios.js
@@ -0,0 +1,14 @@
+//Exercicio 01 
+
+ function fatorial(n) {
+    if (n == 0) {
+        return 1;
+    }
+    var resp = n;
+    while (n > 2) {
+        resp *= --n;
+    }
+    return resp;
+}
+ 
+console.log(fatorial(4));
