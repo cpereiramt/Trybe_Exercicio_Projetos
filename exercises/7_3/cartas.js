0aa31fbff6fec560cea3df4a63728ad591bb265e
Clayton Pereira <cpereiramt@gmail.com>
2020-03-22 18:22:19 -0400

    Salvando as alteraçoes do exercicio, faltando implementar a parte das animaçoes em css

--- exercises/7_3/cartas.js
+++ exercises/7_3/cartas.js
@@ -4,14 +4,10 @@ const pilhacartas = ['nove-de-espadas.png','cinco-de-ouros.png', 'seis-de-copas.
 
 cartaverso.addEventListener('click', function () {
  const gridcartas = document.querySelectorAll('.gridcartas');
- if (gridcartas[0].childElementCount < 7){
+
   imgcarta = document.createElement('img')
   imgcarta.setAttribute('class','cartas');
   imgcarta.setAttribute('src', pilhacartas[Math.floor(Math.random() * 7)]);
    gridcartas[0].appendChild(imgcarta); 
    console.log(Math.floor(Math.random() * 7));    
- 
- } else {
-   alert("Numero máximo de cartas excedido ! favor excluir uma carta para poder inserir novas !") ;
- }
 });
