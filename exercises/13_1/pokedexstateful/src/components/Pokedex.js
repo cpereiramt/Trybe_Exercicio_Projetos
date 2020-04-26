04a17a2ca611678543458ac9e3e6c3c3ab1929b4
Clayton Pereira <cpereiramt@gmail.com>
2020-04-26 17:37:52 -0400

    Salvando resoluçao do exercicio do bloco 13.1

--- exercises/13_1/pokedexstateful/src/components/Pokedex.js
+++ exercises/13_1/pokedexstateful/src/components/Pokedex.js
@@ -1,15 +1,34 @@
 import Pokemon from './Pokemon'
 import React from 'react'
+import Button from './Button'
 
 
-function Pokedex(props) {   
-     
-    return (
-      <div className="pokemon-container">
-      <Pokemon data={props.data}/>
-     </div>
-  )
-
+class Pokedex extends React.Component { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemons : props.data,
+      index : 0
+    }
+   } 
+   
+   navegaPokemons = () => {
+     while(this.state.pokemons.length > this.state.index){
+      console.log(this.state.index,"indice ");
+      console.log(this.state.pokemons, "pokemons");
+     }
+   }
+   
+     render () {
+      return (
+        <div className="pokemon-container">
+        <Pokemon data={this.state.pokemons}/>
+        <Button event={this.navegaPokemons}/>
+       </div>
+    )
+  }
+    
+  
 }
 
 
--- exercises/13_1/pokedexstateful/src/components/Pokemon.js
+++ exercises/13_1/pokedexstateful/src/components/Pokemon.js
@@ -4,27 +4,26 @@ import './pokemon.css';
 
 
 
-function Pokemon(props) {
-       const fields = props.data.map(element => {
-      return(
-          <div className='pokemon'>
-        <div className='pokemon-labels'>            
-        <label>Name: {element.name} </label>
-        <br />
-        <label>Type:  {element.type} </label>
-        <br />
-        <label>Weight:  {element.averageWeight.value} {element.averageWeight.measurementUnit}</label>
-        <br />
-        </div>
-        <div className='pokemon-image'>
-        <img className="imgPokemon" src={element.image} alt={`id from pokemon` + element}></img>  
-        </div>
-        </div>
-          )
-    })
-  return (
-    <div className='pokemon-list'>  
-    {fields}
-    </div>)}
+class Pokemon extends React.Component {
+ 
 
+     render() {
+       return (
+            <div className='pokemon'>
+          <div className='pokemon-labels'>            
+          <label>Name: {this.props.data[0].name} </label>
+          <br />
+          <label>Type:  {this.props.data[0].type} </label>
+          <br />
+          <label>Weight:  {this.props.data[0].averageWeight.value} {this.props.data[0].averageWeight.measurementUnit}</label>
+          <br />
+          </div>
+          <div className='pokemon-image'>
+          <img className="imgPokemon" src={this.props.data[0].image} alt={`id from pokemon` + this.props.data[0]}></img>  
+          </div>
+          </div>
+            )
+      }
+    }
+       
   export default Pokemon;
