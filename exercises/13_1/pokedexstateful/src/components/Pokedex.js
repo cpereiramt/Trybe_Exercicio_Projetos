b5ed340ee77aba2a6764fcdad0490d56ff87ca2b
Clayton Pereira <cpereiramt@gmail.com>
2020-04-26 21:52:34 -0400

    Salvando as alteraçoes no projeto

--- exercises/13_1/pokedexstateful/src/components/Pokedex.js
+++ exercises/13_1/pokedexstateful/src/components/Pokedex.js
@@ -10,24 +10,28 @@ class Pokedex extends React.Component {
     this.state = {
       pokemons : props.data,
       index : 0,
-      currentElements:[props.data]
+      currentElements:'',
+      pokemonsFiltrados:[props.data]
     }
    } 
    
    filtraPokemon = (Type) => {
-    this.setState({pokemons:this.state.pokemons.filter(element => element.type === Type)})
+     if(Type === "all") {
+      this.setState({pokemonsFiltrados:this.props.data})
+     }
+    this.setState({pokemonsFiltrados:this.state.pokemons.filter(element => element.type === Type)})
    }
 
    navegaPokemons = () => {
-   if(this.state.index < this.state.pokemons.length - 1) {
+   if(this.state.index < this.state.pokemonsFiltrados.length - 1) {
     this.setState((state) => ({
       index: state.index + 1
     }));  
-   this.setState({currentElements:this.state.pokemons[this.state.index]}) 
+   this.setState({currentElements:this.state.pokemonsFiltrados[this.state.index]}) 
 
    } else {
     this.setState({index: 0})
-    this.setState({currentElements:this.state.pokemons[this.state.index]})    
+    this.setState({currentElements:this.state.pokemonsFiltrados[this.state.index]})    
    }   
   }   
      render () {
@@ -37,7 +41,8 @@ class Pokedex extends React.Component {
         <Pokemon data={this.state.currentElements}/>
         <ButtonNext navegar={this.navegaPokemons} name="Proximo"/>
         <ButtonPokemonType filtrar={this.filtraPokemon} name="Fire"/>
-        <ButtonPokemonType filtrar={this.filtraPokemon} name="Psychic"/>        
+        <ButtonPokemonType filtrar={this.filtraPokemon} name="Psychic"/>
+        <ButtonPokemonType filtrar={this.filtraPokemon} name="all"/>      
        </div>
     )
   }   
